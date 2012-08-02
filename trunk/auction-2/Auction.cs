e47e37286b4e6ec89ba3266051bd247f0534ccc0
pkartsel@gmail.com <pkartsel@gmail.com@39e89425-0a54-b93a-e518-47c00aa92f8f>
2012-08-02 14:16:42 +0000

    Вроде что-то работает. Без шаблонов, ивентов и всякой другой ненужности

--- trunk/auction-2/Auction.cs
+++ trunk/auction-2/Auction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -23,9 +24,9 @@ namespace auction_2
         public ReadOnlyCollection<Category> Categories {get {return new ReadOnlyCollection<Category>(_categories); } }
         public ReadOnlyCollection<Seller> Sellers {get{return new ReadOnlyCollection<Seller>(_sellers);}} 
         public ReadOnlyCollection<Buyer> Buyers {get {return new ReadOnlyCollection<Buyer>(_buyers);}}
-
         public AuctionSettings Settings { get; private set; }
 
+        
         public Auction()
         {
             _bids = new List<Bid>();
@@ -39,10 +40,101 @@ namespace auction_2
             Settings = new AuctionSettings();
 
         }
-        
-        public void MakeBid()
+
+        public void AddBuyer(Buyer buyer)
         {
-            
+            if (_buyers.Count(b => b.Login == buyer.Login) == 0)
+            {
+                _buyers.Add(buyer);
+            }
+        }
+        public void AddSeller(Seller seller)
+        {
+            if (_sellers.Count(s => s.Login == seller.Login) == 0)
+                _sellers.Add(seller);
+        }
+        public void AddCategory(Category category)
+        {
+            if (_categories.Count(c => c.Name == category.Name) == 0)
+            {
+                _categories.Add(category);
+            }
+        }
+        public void AddSeries(Series series)
+        {
+            if (_series.Count(s => s.Name == series.Name) == 0)
+            {
+                _series.Add(series);
+            }
+        }
+        public void AddSale(Sale sale)
+        {
+            if (IsCorrectSale(sale))
+            {
+                AddLot(sale.Lot);
+                _sales.Add(sale);
+
+                var lastOrDefault = _sales.LastOrDefault();
+                if (lastOrDefault != null) sale.Number = lastOrDefault.Number + 1;
+
+                if (sale.Duration < Settings.MinSaleDuration)
+                {
+                    sale.Duration = Settings.MinSaleDuration;
+                }
+            }
+
+        }
+        public void AddLot(Lot lot)
+        {
+            if (!_lots.Contains(lot))
+            {
+                _lots.Add(lot);
+            }
+        }
+
+        public IEnumerable<Sale> GetSuccessfulSales()
+        {
+            return _sales.Where(s => s.IsSaled);
+        }
+        public IEnumerable<Sale> GetSuccessfulSales(Buyer buyer)
+        {
+            return _sales.Where(s => s.IsSaled && s.Buyer == buyer);
+        }
+        public IEnumerable<Sale> GetSuccessfulSales(Seller seller)
+        {
+            return _sales.Where(s => s.IsSaled && s.Seller == seller);
+        }
+
+
+        public void MakeBid(Bid bid)
+        {
+            if (IsCorrectBid(bid))
+            {
+                _bids.Add(bid);
+                bid.Sale.LastBid = bid;
+            }
+        }
+
+        // возвращает сумму успешных покупок
+        private double GetBuyerIndex(Buyer buyer)
+        {
+            return GetSuccessfulSales(buyer).Sum(s => s.CurrentPrice);
+        }
+        private bool IsCorrectBid(Bid bid)
+        {
+            var s = bid.Sale;
+            return s.IsActive &&
+                   (bid.Value - s.Increment >= s.CurrentPrice) &&
+                   s.Category.Restriction <= GetBuyerIndex(bid.Bidder) &&
+                   (s.LastBid == null || s.LastBid.Bidder != bid.Bidder);
+
+        }
+
+        //ex ?
+        private bool IsCorrectSale(Sale sale)
+        {
+            return !_sales.Contains(sale) && (_sellers.Contains(sale.Seller) && _series.Contains(sale.Series)) &&
+                   sale.Lot != null;
         }
     }
 }
